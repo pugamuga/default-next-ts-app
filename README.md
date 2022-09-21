cc87b967081ce995bdb1977f168774e5d00ef461
Evgeny Pugachev <rabota.pugamuga@gmail.com>
2022-09-21 17:41:21 +0300

    initial commit

--- README.md
+++ README.md
@@ -9,4 +9,5 @@ added:
 - framer motion
 
 - fonts
-  for adding fonts add url in global.css and add name of font in tailwind.config.js
+  for adding fonts add url in global.css and add name of font in tailwnpm run dev
+  
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 
-const Home: NextPage = () => {
+const Home: NextPage = (): JSX.Element => {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2 ">
       <div className=" flex flex-col items-center border-y-2 border-y-red-500 py-4">
