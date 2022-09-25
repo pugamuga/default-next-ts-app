2e0cd3d8cc5a958be34048a48cb9098e95cebe9a
Evgeny Pugachev <rabota.pugamuga@gmail.com>
2022-09-25 16:03:20 +0300

    add svg component and transition presets in tailwid

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,21 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
+import SvgIcon from "../components/SvgIcon";
 
 const Home: NextPage = (): JSX.Element => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2 ">
-      <div className=" flex flex-col items-center border-y-2 border-y-red-500 py-4">
+    <div className="flex min-h-screen  flex-col items-center justify-center py-2 ">
+      <div className=" flex flex-col relative items-center border-y-2 border-y-red-500 py-4">
         <div className=" text-red-500 text-5xl font-kufiM">PugaMuga</div>
         <div className=" text-white text-5xl font-nablaPixel bg-slate-900 px-4 py-2 my-2">
           PugaMuga
         </div>
         <div className=" text-red-500 text-5xl font-strongItalik">PugaMuga</div>
         <div className=" text-red-500 text-5xl font-lobster">PugaMuga</div>
+        <div className=" absolute top-[-13px] right-[-21px] rotate-90 hover:rotate-0 tr-700">
+          <SvgIcon />
+        </div>
       </div>
     </div>
   );
