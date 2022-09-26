970a3361f1b5d27b17786825fa6c5a4f385426b5
Evgeny Pugachev <rabota.pugamuga@gmail.com>
2022-09-26 10:53:09 +0300

    continue toggle

--- pages/DarkModeToggle.tsx
+++ pages/DarkModeToggle.tsx
@@ -8,10 +8,16 @@ export default function DarkModeToggle(): JSX.Element {
   const [toggle, setToggle] = useState<boolean>(false);
   return (
     <div className="p-10 flex flex-row items-center gap-x-3 bg-slate-500">
-      <h1 className={` font-extrabold text-blue-900`}>Light</h1>
+      <h1
+        className={` font-extrabold  ${
+          !toggle ? "text-slate-800" : "text-slate-800/20"
+        }`}
+      >
+        Light
+      </h1>
       <div
         className={`h-[40px] w-[70px] rounded-full  ${
-          toggle ? "bg-slate-800" : "bg-sky-400"
+          toggle ? "bg-slate-800" : "bg-cyan-300"
         } flex tr-300 items-center`}
       >
         <motion.div
@@ -22,15 +28,47 @@ export default function DarkModeToggle(): JSX.Element {
           animate={{ x: toggle ? 30 : 0 }}
           className="w-6 h-6 cursor-pointer relative"
         >
-          <AnimatePresence>
-            <SvgSun toggle={toggle}/>
+          <AnimatePresence initial={false}>
+            {!toggle && (
+              <>
+                <SvgSun toggle={toggle} />
+                <motion.div
+                  initial={{ x: -60, opacity: 0 }}
+                  animate={{ x: 0, opacity: 1 }}
+                  exit={{ x: -60, opacity: 0 }}
+                  transition={{ duration: 0.1 }}
+                >
+                  <div className="w-2 h-2 absolute right-[-30px] top-[3px] bg-white rounded-full"></div>
+                  <div className="w-1 h-1 absolute right-[-18px] top-[10px] bg-white rounded-full"></div>
+                </motion.div>
+              </>
+            )}
           </AnimatePresence>
-          <AnimatePresence>
-            <SvgMoon toggle={toggle}/>
+          <AnimatePresence initial={false}>
+            {toggle && (
+              <>
+                <SvgMoon toggle={toggle} />
+                <motion.div
+                  initial={{ x: 60, opacity: 0 }}
+                  animate={{ x: 0, opacity: 1 }}
+                  exit={{ x: 60, opacity: 0 }}
+                  transition={{ duration: 0.1 }}
+                >
+                  <div className="w-[2px] h-[2px] absolute right-[30px] top-[10px] bg-white rounded-full"></div>
+                  <div className="w-[2px] h-[2px] absolute right-[40px] top-[20px] bg-white rounded-full"></div>
+                </motion.div>
+              </>
+            )}
           </AnimatePresence>
         </motion.div>
       </div>
-      <h1 className={` font-extrabold text-blue-900`}>Dark</h1>
+      <h1
+        className={` font-extrabold ${
+          toggle ? "text-slate-800" : "text-slate-800/20"
+        }`}
+      >
+        Dark
+      </h1>
     </div>
   );
 }
